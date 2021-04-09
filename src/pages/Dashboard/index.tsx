98b805b51b880f13db37742e1bc95bebe1757d45
Dostoievsky <bernardo.emery@gmail.com>
2021-04-09 16:44:08 -0300

    Feature: first commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// import { Container, Content } from './styles';
+
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+          <h1>HELLO WORLD!</h1>
+    </>
+  );
+};
+
+export default Dashboard;
