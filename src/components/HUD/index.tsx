afd985c6f1f38c3c4ac5c102e945df9a1ec59865
Dostoievsky <bernardo.emery@gmail.com>
2021-04-10 16:36:07 -0300

    Feature: initial commit for validation page

--- src/components/HUD/index.tsx
+++ src/components/HUD/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import InputContainer from '../InputContainer';
+import InputContainer from '../ButtonContainer';
 
 import { Container, Background } from './styles';
 
---
+++ src/pages/validate/index.tsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState} from 'react';
+import { useHistory } from "react-router-dom";
+
+import {useInput} from '../../hooks/input';
+
+import { Container } from '../Landing/styles';
+
+const Validate: React.FC = () => {
+  const {message, addMessage} = useInput();
+  const history = useHistory();
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if(message !== ''){
+      switch (message) {
+        
+        case 'B': {
+          addMessage('');
+          history.push('/');
+          break;
+        }
+
+        default: {
+          setError('OPÇÃO INVALIDA TENTE NOVAMENTE!');
+        }
+      }
+    }
+  }, [message, history, setError, addMessage]);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setError('');
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [setError]);
+
+  return (
+    <Container>
+      <h1>Validação</h1>
+      <h1>Insira o seu CPF:</h1>   
+      <h2>{error}</h2>
+    </Container>
+  );
+};
+
+export default Validate;
