5081f57b60b8feb29cf878b7dfdbd0fc6c653b7f
Dostoievsky <bernardo.emery@gmail.com>
2021-04-11 17:14:16 -0300

    Feature: almost finished candidate list

--- src/pages/endScreen/index.tsx
+++ src/pages/endScreen/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import {TiBatteryCharge} from 'react-icons/ti';
 
 import { Container } from './styles';
-
+//BsBatteryHalf
 const EndEscreen: React.FC = () => {
+  const today = new Date();
+  let date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
   return (
     <Container>
-      <h1>DIA XX/XX/XXXX XX:XX:XX</h1>
-      <img src="./bateria.svg" alt=""/>
+      <h1>{date} {time}</h1>
+      <TiBatteryCharge/>
       <h2>Votou</h2>
       <h3>FIM</h3>
     </Container>
